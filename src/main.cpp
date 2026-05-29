d27715fa25e54fd0d20f0947bedbf078ef7a6baa
Shoham Ben-Yosef <Shohamby1@gmail.com>
2026-05-29 15:28:08 +0300

    Add interactive CLI menu

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,25 @@
 #include <limits>
 
 
+int readInt(const std::string& prompt) {
+    int val;
+
+    while (true) {
+        std::cout << prompt;
+
+        if (std::cin >> val) {
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            return val;
+        }
+        std::cout << "Invalid input. Please enter a number.\n";
+
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    
+}
+
+
 int main() {
     
     TaskManager manager;
@@ -17,15 +36,15 @@ int main() {
         std::cout << "3. Mark completed\n";
         std::cout << "4. Print all\n";
         std::cout << "0. Exit\n";
-        std::cout << "Choice: ";
 
-
-        std::cin >> choice;
+        choice = readInt("Choice: ");
 
         switch (choice) {
-            case 0: 
+            case 0: {
                 std::cout << "Exit\n";
                 return 0;
+            }
+
             case 1: {
                 std::string title;
 
@@ -56,26 +75,26 @@ int main() {
             }
 
             case 3: {
-                int id;
-                std::cout << "Mark completed\nEnter Task ID:\n";
-                std::cin >> id;
+                int id = readInt("Mark completed\nEnter Task ID:\n");
 
-                if (manager.markTaskCompleted(id)){
+                if (manager.markTaskCompleted(id)) {
                     std::cout << "Task marked as completed.\n";
                 } else {
-                    std::cout <<"Task not found\n";
+                    std::cout << "Task not found.\n";
                 }
-                
+
                 break;
             }
 
-            case 4:
+            case 4: {
                 manager.printAllTasks();
                 break;
+            }
 
-            default:
+            default: {
                 std::cout << "Invalid choice\n";
                 break;
+            }
         }
     }
 
